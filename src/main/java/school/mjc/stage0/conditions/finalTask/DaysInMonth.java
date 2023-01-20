2211567681601daa12d6c29fcab6978260f940ae
asha <asha88ptz@gmail.com>
2023-01-20 12:38:25 +0300

    Switch tasks

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        int amount;
+        switch (month) {
+            case 2:
+                boolean isLeap = ((year % 4 == 0) && (year % 100) != 0) || (year % 400 == 0);
+                amount = isLeap ? 29 : 28;
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                amount = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                amount = 30;
+                break;
+            default:
+                System.out.println("invalid date");
+                return;
+
+        }
+        System.out.println(amount);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,21 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
+    /**
+     * "this is a valid triangle"
+     * "it's not a triangle"
+     */
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+            System.out.println("it's not a triangle");
+        } else if (firstSide + secondSide < thirdSide) {
+            System.out.println("it's not a triangle");
+        } else if (secondSide + thirdSide < firstSide) {
+            System.out.println("it's not a triangle");
+        } else if (thirdSide + firstSide < secondSide) {
+            System.out.println("it's not a triangle");
+        } else {
+            System.out.println("this is a valid triangle");
+        }
     }
 }
